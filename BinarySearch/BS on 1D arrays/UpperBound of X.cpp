d4680ca32a3c4f7693258f37fb917c4fefe22514
Dinesh <dineshreddy13077@gmail.com>
2026-06-15 13:05:39 +0530

    Updated Time and Space Complexity

--- BinarySearch/BS on 1D arrays/UpperBound of X.cpp
+++ BinarySearch/BS on 1D arrays/UpperBound of X.cpp
@@ -21,5 +21,11 @@ public:
             }
         }
         return ans;
+        /*
+          Time Complexity->Brute->O(n)
+                         ->Optimal->O(log n base 2)
+          Space Complexity->O(1)
+        */
+
     }
 };
